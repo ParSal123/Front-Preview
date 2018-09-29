7dc1aaa15c8a50dbec8c587aeee6c8824401aa3f
Kiarash Golzadeh <kiarash.gl@gmail.com>
2018-09-29 14:14:40 +0330

    Fixed Locker Modal Titles Fixed Locker Names

---
+++ js/locker.js
@@ -0,0 +1,11 @@
+var lockerLetter = 'M';
+
+function changeLetter(ch) {
+    $("#locker-title").html("کمد "+ch);
+    lockerLetter = ch;
+    $(".locker > .row > .col ").each(function () {
+        var my  = $(this).html();
+        my = my.slice(my.length-2,my.length);
+        $(this).html(lockerLetter+my);
+    });
+}
